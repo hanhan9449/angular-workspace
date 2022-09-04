c806b019f2e788b59dc25f2c662c189eb4246d89
Han Han <haannn@qq.com>
2022-09-04 06:36:42 +0000

    feat: 初始化页面

--- projects/blog/src/app/app-routing.module.ts
+++ projects/blog/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'post',
+    loadChildren: () => import('./post/post.module').then(m => m.PostModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
