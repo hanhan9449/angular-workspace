f0595e68cef333b3aa1e019bd6f9af32f1f25c7d
Han Han <haannn@qq.com>
2022-09-05 14:38:50 +0000

    feat: add @angular/fire

--- projects/blog/src/app/app.module.ts
+++ projects/blog/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VendorsModule } from 'projects/vendors/src/public-api';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,9 +21,17 @@ import { VendorsModule } from 'projects/vendors/src/public-api';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    VendorsModule
+    VendorsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- projects/blog/src/environments/environment.prod.ts
+++ projects/blog/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-workspace-511e0',
+    appId: '1:403351094816:web:a5434bfd2f1b791f8c9e45',
+    storageBucket: 'angular-workspace-511e0.appspot.com',
+    locationId: 'asia-east2',
+    apiKey: 'AIzaSyChuJNYbGFEIDNxG1EBVpy5IVZigT6PSLw',
+    authDomain: 'angular-workspace-511e0.firebaseapp.com',
+    messagingSenderId: '403351094816',
+    measurementId: 'G-64G8K0EE22',
+  },
   production: true
 };
