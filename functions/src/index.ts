8dd7a8a40ccb8c264fc675b4766a2a41e2f0681c
hanhan.rich <haannn@qq.com>
2022-09-08 00:18:57 +0800

    feat: 使用functions自动生成文章description

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,18 @@
 import * as functions from "firebase-functions";
 
+
+export const updatePostDescription = functions.firestore.document('post/{postId}').onWrite((change, context) => {
+  const {before, after} = change
+  const bContent = before.data()?.content
+  const aContent = after.data()?.content
+  functions.logger.info('before content', bContent)
+  functions.logger.info('after content', aContent)
+  functions.logger.debug('isEqual', bContent === aContent)
+  if (bContent !== aContent) {
+    return after.ref.set({description: (aContent ?? '').slice(0, 200)}, {merge: true})
+  }
+  return;
+})
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
