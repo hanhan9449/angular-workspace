c558c034239ce029b5d59e23dcc1f2f8f3e25167
hanhan.rich <haannn@qq.com>
2022-09-10 14:47:10 +0800

    fix: 二维码工具图片闪烁

--- projects/tools/src/lib/qr-code/qr-code.component.ts
+++ projects/tools/src/lib/qr-code/qr-code.component.ts
@@ -1,24 +1,26 @@
 import {AfterContentChecked, AfterViewChecked, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {QrCodeService} from "./qr-code.service";
-import {Observable} from "rxjs";
+import {BehaviorSubject, concatMap, mergeMap, Observable, of, Subject, switchMap, tap} from "rxjs";
 
 @Component({
   selector: 'lib-qr-code',
   templateUrl: './qr-code.component.html',
   styleUrls: ['./qr-code.component.scss']
 })
-export class QrCodeComponent implements OnChanges {
+export class QrCodeComponent implements OnChanges, OnInit {
   @Input() text?: string;
+  text$ = new BehaviorSubject<string>('')
   src$?: Observable<string>
 
   constructor(public qrCodeService: QrCodeService) { }
+  ngOnInit(): void {
+    this.src$ = this.text$
+      .pipe(
+        concatMap(t => (this.qrCodeService.geneImageFromLocal(t))))
+  }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (!this.text) {
-      return;
-    }
-    this.src$ =  this.qrCodeService.geneImageFromLocal(this.text)
-
+    this.text$.next(this.text ?? '')
   }
 
 }
