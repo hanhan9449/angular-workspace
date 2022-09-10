f4324d603f04b91754bd747ddaef30d15596e109
hanhan.rich <haannn@qq.com>
2022-09-10 18:42:10 +0800

    fix: 无法打开nav list

--- projects/vendors/src/lib/header/header.component.ts
+++ projects/vendors/src/lib/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import { CONSTANTS } from 'projects/shared/src/constants';
 import { of, Subject } from 'rxjs';
 import { navItemList } from './nav-item.list.config';
@@ -9,16 +9,26 @@ import { navItemList } from './nav-item.list.config';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('menu', {read: ElementRef, static: false}) menuEl?: ElementRef<HTMLButtonElement>
 
   title = CONSTANTS.WEBSITE_TITLE
   subtitle = CONSTANTS.WEBSITE_SUBTITLE
   hideSubtitle = true
   navItemList$ = of(navItemList)
+  showNavList = false
 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:click', ['$event.target'])
+  handleDocumentClick(target: HTMLElement) {
+    if ( this.menuEl?.nativeElement.contains(target) ) {
+      return;
+    }
+    this.showNavList = false
   }
 
 }
