c489b4f604f63e6e8a732175dffd48e4f6a56246
hanhan.rich <haannn@qq.com>
2022-09-12 17:25:46 +0800

    feat: 使用模态框透出firebaseui

--- projects/blog/src/app/admin/admin.component.ts
+++ projects/blog/src/app/admin/admin.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "service";
 import {Observable} from "rxjs";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from "@angular/material/dialog";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {SignInDialogComponent} from "./sign-in-dialog/sign-in-dialog.component";
 
 @Component({
   selector: 'app-admin',
@@ -15,13 +18,16 @@ export class AdminComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
     this.isSignIn$ = this.authService.isSignIn$
   }
   signIn() {
+    const dialogRef = this.dialog.open(SignInDialogComponent)
+
   }
   async signOut() {
     await this.authService.signOut()
