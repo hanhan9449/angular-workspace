02a8e5cf7c223683a792d68180fc9c82601f263b
hanhan.rich <haannn@qq.com>
2022-09-12 13:59:22 +0800

    feat: 首页建设

--- projects/blog/src/app/home/home.component.ts
+++ projects/blog/src/app/home/home.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = '首页'
+  title = '首页（建设中）'
+  text = `
+      hi，你好！
+
+      这是hanhan9449的网站，还在建设基础能力中。。。
+
+      - 想看看我写的文章就跳转到**文章**看看
+      - 想看看快捷的工具就跳转到**工具**看看
+      - 想了解我就跳转到**我的**看看
+
+      后面会写一些关于博客建设的文章，期待吧QAQ
+  `
 
   constructor() { }
 
