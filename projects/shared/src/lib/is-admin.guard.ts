0b77f6706b6c8cfb93f0d112ca21d439cd0400bb
hanhan.rich <haannn@qq.com>
2022-09-13 22:29:17 +0800

    feat: 管理员权限支持

---
+++ projects/shared/src/lib/is-admin.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {map, Observable} from 'rxjs';
+import {AuthService} from "service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsAdminGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isAdmin$.pipe(map(b => {
+      if (b) {
+        return b
+      }
+      return this.router.parseUrl('/no-auth')
+    }))
+  }
+
+}
